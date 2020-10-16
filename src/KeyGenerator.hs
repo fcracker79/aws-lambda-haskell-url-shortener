ea9d6a08c96038e2e84cabf8b29bbfdc05411a5e
fcracker79 <fcracker79@gmail.com>
2020-10-16 08:00:26 +0200

    Key generation OK.

--- src/KeyGenerator.hs
+++ src/KeyGenerator.hs
@@ -3,13 +3,17 @@ module KeyGenerator where
 import Utils
 import Test.RandomStrings
 
-urlPathChars = onlyAlphaNum randomASCII
+_minLength = 4
+_urlPathChars = onlyAlphaNum randomASCII
 
-numberOfAttempts :: Int -> Int
-numberOfAttempts len = 10 ^ len
+_numberOfAttempts :: Int -> Int
+_numberOfAttempts len = 10 ^ len
 
 _generateKeysByLen :: Int -> [IO String]
-_generateKeysByLen len = replicate (numberOfAttempts len) $ monadicFirst $ randomStringsLen (randomString urlPathChars) (len, len) 1
+_generateKeysByLen len = replicate (_numberOfAttempts len) $ monadicFirst $ randomStringsLen (randomString _urlPathChars) (len, len) 1
 
--- generateKeys :: [IO String]
--- generateKeys = foldM (++) (IO []) fmap _generateKeysByLen [1..]
+_recGenerateKeys :: Int -> [IO String]
+_recGenerateKeys i = (_generateKeysByLen i) ++ (_recGenerateKeys (i + 1))
+
+generateKeys :: [IO String]
+generateKeys = _recGenerateKeys _minLength
