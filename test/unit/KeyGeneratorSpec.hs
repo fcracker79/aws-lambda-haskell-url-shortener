c21104a937445697ab3f50f0311312b18a60e687
fcracker79 <fcracker79@gmail.com>
2020-10-18 07:51:23 +0200

    Added a bit of unit tests to key generations.

--- test/unit/KeyGeneratorSpec.hs
+++ test/unit/KeyGeneratorSpec.hs
@@ -9,4 +9,9 @@ spec = do
   describe "Prelude.head" $ do
     it "The first keys must be 4 chars long" $ do
       keys <- generateKeys
-      length (keys!!0) `shouldBe` 4
+      let first1000Keys = take 1000 keys
+      mconcat $ fmap (\x -> length x `shouldBe` 4) $ first1000Keys
+    it "The 100000th element must be 5 chars long" $ do
+      keys <- generateKeys
+      length (keys!!9999) `shouldBe` 4
+      length (keys!!10000) `shouldBe` 5
