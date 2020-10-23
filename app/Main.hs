b8fd93f53d6fa4921c1cc42e62fbfa34e94dbf83
fcracker79 <fcracker79@gmail.com>
2020-10-23 08:53:09 +0200

    Added dependency from aws-lambda-haskell-runtime personal repo. This allows custom entry point implementation.

--- app/Main.hs
+++ app/Main.hs
@@ -5,12 +5,62 @@
 module Main where
 
 import Aws.Lambda
-
+import Aws.Lambda.Runtime.Common( LambdaError(..) )
+import Control.Exception.Base
 import qualified Lib
+import Data.Text
+import GHC.Exception.Type (SomeException)
 
 -- This action is run on each cold start, and the context returned
 -- is kept alive while the lambda is run, you can access it in your handler.
 initializeContext :: IO ()
 initializeContext = return ()
 
-generateLambdaDispatcher UseWithAPIGateway defaultDispatcherOptions
+-- generateLambdaDispatcher UseWithAPIGateway defaultDispatcherOptions
+main = (runLambda initializeContext) run
+run
+ LambdaOptions {functionHandler = functionHandler,
+                contextObject = contextObject, eventObject = eventObject,
+                executionUuid = executionUuid}
+ = case functionHandler of
+     "src/Lib.handler"
+       -> do let returnErr_aIUv statusCode_aIUw
+                   = (pure
+                        . (Left
+                             . (ApiGatewayLambdaError
+                                  . mkApiGatewayResponse statusCode_aIUw)))
+             case decodeObj eventObject of
+               Right eventObject_aIUx
+                 -> do resultE_aIUy <- (try
+                                          $ (Lib.handler eventObject_aIUx) contextObject)
+                       case resultE_aIUy of
+                         Right result_aIUz
+                           -> ((either
+                                  (pure
+                                     . (Left
+                                          . (ApiGatewayLambdaError
+                                               . fmap toApiGatewayResponseBody))))
+                                 (pure
+                                    . (Right
+                                         . (ApiGatewayResult
+                                              . fmap toApiGatewayResponseBody))))
+                                result_aIUz
+                         Left (handlerError_aIUA :: SomeException)
+                           -> if ((propagateImpureExceptions . apiGatewayDispatcherOptions)
+                                    $ DispatcherOptions
+                                        (ApiGatewayDispatcherOptions True)) then
+                                  ((returnErr_aIUv 500 . (toApiGatewayResponseBody . show))
+                                     $ handlerError_aIUA)
+                              else
+                                  ((returnErr_aIUv 500
+                                      . (toApiGatewayResponseBody . pack))
+                                     $ "Something went wrong.")
+               Left err_aIUB
+                 -> ((returnErr_aIUv 400 . (toApiGatewayResponseBody . show))
+                       $ err_aIUB)
+     _ -> ((pure
+              . (Left
+                   . (ApiGatewayLambdaError
+                        . (mkApiGatewayResponse 500 . toApiGatewayResponseBody))))
+             $ ("Handler "
+                  <> (functionHandler <> " does not exist on project")))
