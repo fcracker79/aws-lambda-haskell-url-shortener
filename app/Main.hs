bdae2d95127590db2f3379e06073e3c456e87a61
fcracker79 <fcracker79@gmail.com>
2020-10-26 08:56:04 +0100

    GetUrl compiles

--- app/Main.hs
+++ app/Main.hs
@@ -16,51 +16,59 @@ import GHC.Exception.Type (SomeException)
 initializeContext :: IO ()
 initializeContext = return ()
 
--- generateLambdaDispatcher UseWithAPIGateway defaultDispatcherOptions
-main = (runLambda initializeContext) run
-run
+
+_run
  LambdaOptions {functionHandler = functionHandler,
                 contextObject = contextObject, eventObject = eventObject,
-                executionUuid = executionUuid}
- = case functionHandler of
-     "src/Lib.handler"
-       -> do let returnErr_aIUv statusCode_aIUw
+                executionUuid = executionUuid} fh = do
+    let returnErr_aIUv statusCode_aIUw
                    = (pure
                         . (Left
                              . (ApiGatewayLambdaError
                                   . mkApiGatewayResponse statusCode_aIUw)))
-             case decodeObj eventObject of
-               Right eventObject_aIUx
-                 -> do resultE_aIUy <- (try
-                                          $ (Lib.handler eventObject_aIUx) contextObject)
-                       case resultE_aIUy of
-                         Right result_aIUz
-                           -> ((either
-                                  (pure
-                                     . (Left
-                                          . (ApiGatewayLambdaError
-                                               . fmap toApiGatewayResponseBody))))
-                                 (pure
-                                    . (Right
-                                         . (ApiGatewayResult
-                                              . fmap toApiGatewayResponseBody))))
-                                result_aIUz
-                         Left (handlerError_aIUA :: SomeException)
-                           -> if ((propagateImpureExceptions . apiGatewayDispatcherOptions)
-                                    $ DispatcherOptions
-                                        (ApiGatewayDispatcherOptions True)) then
-                                  ((returnErr_aIUv 500 . (toApiGatewayResponseBody . show))
-                                     $ handlerError_aIUA)
-                              else
-                                  ((returnErr_aIUv 500
-                                      . (toApiGatewayResponseBody . pack))
-                                     $ "Something went wrong.")
-               Left err_aIUB
-                 -> ((returnErr_aIUv 400 . (toApiGatewayResponseBody . show))
-                       $ err_aIUB)
-     _ -> ((pure
-              . (Left
-                   . (ApiGatewayLambdaError
-                        . (mkApiGatewayResponse 500 . toApiGatewayResponseBody))))
-             $ ("Handler "
-                  <> (functionHandler <> " does not exist on project")))
+    case decodeObj eventObject of
+       Right eventObject_aIUx
+         -> do resultE_aIUy <- (try
+                                  $ (fh eventObject_aIUx) contextObject)
+               case resultE_aIUy of
+                 Right result_aIUz
+                   -> ((either
+                          (pure
+                             . (Left
+                                  . (ApiGatewayLambdaError
+                                       . fmap toApiGatewayResponseBody))))
+                         (pure
+                            . (Right
+                                 . (ApiGatewayResult
+                                      . fmap toApiGatewayResponseBody))))
+                        result_aIUz
+                 Left (handlerError_aIUA :: SomeException)
+                   -> if ((propagateImpureExceptions . apiGatewayDispatcherOptions)
+                            $ DispatcherOptions
+                                (ApiGatewayDispatcherOptions True)) then
+                          ((returnErr_aIUv 500 . (toApiGatewayResponseBody . show))
+                             $ handlerError_aIUA)
+                      else
+                          ((returnErr_aIUv 500
+                              . (toApiGatewayResponseBody . pack))
+                             $ "Something went wrong.")
+       Left err_aIUB
+         -> ((returnErr_aIUv 400 . (toApiGatewayResponseBody . show))
+               $ err_aIUB)
+
+
+-- generateLambdaDispatcher UseWithAPIGateway defaultDispatcherOptions
+main = (runLambda initializeContext) run
+run LambdaOptions {functionHandler = functionHandler,
+                contextObject = contextObject, eventObject = eventObject,
+                executionUuid = executionUuid}
+    = case functionHandler of
+        "test" -> _run LambdaOptions {functionHandler = functionHandler,
+                                                  contextObject = contextObject, eventObject = eventObject,
+                                                  executionUuid = executionUuid} Lib.handler
+        _ -> ((pure
+                 . (Left
+                      . (ApiGatewayLambdaError
+                           . (mkApiGatewayResponse 500 . toApiGatewayResponseBody))))
+                $ ("Handler "
+                     <> (functionHandler <> " does not exist on project")))
---
+++ src/AWSFunctions.hs
@@ -0,0 +1,51 @@
+module AWSFunctions where
+
+
+import GHC.Generics
+import Aws.Lambda
+import Network.HTTP.Types.Header
+import Data.ByteString.Char8
+import System.Environment as Sysenv
+import Data.Text
+import URLGenerator
+import DynamoDB
+import Network.AWS
+import Network.AWS.Data
+
+_getConf :: IO DynamoDBConfiguration
+_getConf = do
+    region <- Sysenv.getEnv "AWS_REGION"
+    return (DynamoDBConfiguration {
+        region=read region::Region,
+        table=DynamoDBTable {
+            tablename="Urls",
+            keyField=Data.Text.pack "id",
+            valueField=Data.Text.pack "url"
+        }
+        })
+
+
+redirectUrl :: (ApiGatewayRequest String) -> Context () -> IO (Either (ApiGatewayResponse String) (ApiGatewayResponse String))
+redirectUrl request context = do
+    conf <- _getConf
+    let shortenerId = Prelude.head $ Prelude.filter
+            (\x -> Data.Text.length x > 0)
+            $ Data.Text.splitOn (Data.Text.pack "/") $ apiGatewayRequestPath request
+    maybeUrl <- URLGenerator.getURL conf $ Data.Text.unpack shortenerId
+    case maybeUrl of
+        Just url -> return $ Right $ ApiGatewayResponse {
+                                  apiGatewayResponseStatusCode = 301,
+                                  apiGatewayResponseIsBase64Encoded = False,
+                                  apiGatewayResponseBody = "",
+                                  apiGatewayResponseHeaders = [
+                                      (hLocation, Data.ByteString.Char8.pack url)
+                                  ]
+                          }
+        Nothing -> return $ Right $ ApiGatewayResponse {
+                                 apiGatewayResponseStatusCode = 404,
+                                 apiGatewayResponseIsBase64Encoded = False,
+                                 apiGatewayResponseBody = "",
+                                 apiGatewayResponseHeaders = [
+
+                                 ]
+                         }
