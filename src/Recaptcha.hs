ede961d2aa12ad5038380c2b206eabb4beed9bdd
fcracker79 <fcracker79@gmail.com>
2020-10-29 08:47:10 +0100

    Req finally working. TODO study lens

--- src/Recaptcha.hs
+++ src/Recaptcha.hs
@@ -3,27 +3,29 @@ module Recaptcha where
 
 {-# LANGUAGE OverloadedStrings #-}
 
-import qualified Data.ByteString.Lazy.Char8 as L8
-import           Data.Aeson            (Value)
-import Network.Wreq
-import Control.Lens
-import OpenSSL.Session (context)
-import Network.HTTP.Client.OpenSSL
+import Control.Monad.IO.Class
+import Data.Aeson
+import Network.HTTP.Req
 import Data.Text as DT
+import Network.HTTP.Req (ReqBodyUrlEnc (..), ReqBodyMultipart, reqBodyMultipart)
+import Network.HTTP.Client.MultipartFormData ( partBS )
+import Data.ByteString.Char8 as BS
+
 
 type RecaptchaSecretKey = String
 type RecaptchaToken = String
 
 
-verifyCaptcha :: RecaptchaSecretKey -> RecaptchaToken -> IO Bool
+
+verifyCaptcha :: Network.HTTP.Req.MonadHttp m => RecaptchaSecretKey -> RecaptchaToken -> m Bool
 verifyCaptcha secretKey token = do
-    let opts = defaults & manager .~ Left (opensslManagerSettings context)
-    r2 <- post "http://httpbin.org/post" ["num" := 3, "str" := "wat"]
-    r <- withOpenSSL $
-         postWith
-             opts
-             "https://www.google.com/recaptcha/api/siteverify"
-             ["secret" := secretKey, "response" := token]
-             --[partText (DT.pack "secret") (DT.pack secretKey), partText (DT.pack "response") (DT.pack token)]
+    response <-
+        req
+          POST
+          (https "www.google.com" /: "recaptcha/api/siteverify")
+          (ReqBodyUrlEnc $ "secret" =: secretKey <> "response" =: token)
+          jsonResponse
+          mempty
+    let jsonResponse = (responseBody response :: Value)
     -- TODO
     return True
